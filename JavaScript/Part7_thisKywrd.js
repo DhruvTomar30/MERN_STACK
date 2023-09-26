c9a87f5cee4c601c97c06a1f376e55c402015a65
dhruvtomar2020@gmail.com <Dhruv Tomar>
2023-09-26 12:56:47 +0530

    DOM

---
+++ JavaScript/Part7_thisKywrd.js
@@ -0,0 +1,33 @@
+const Student={
+    name:"dhruv",
+    age:20,
+    eng:96,
+    phy:98,
+    maths:92,
+    getAvg(){
+        console.log(this);
+        let avg=(maths+phy+eng)/3;
+        console.log(`${this.name} got avg marks = ${avg}`);
+    }
+}
+
+// Arrow Function....
+const sum=(a,b)=> {
+ console.log(a+b);}
+
+ "or"
+ const mul=(a,b)=>a*b;
+
+//  SetTimeout Function...
+setTimeout(()=>{
+ console.log("apna College");
+},2000);
+console.log("welcome to");
+
+
+// Set Interval....
+let id=setInterval(()=>{
+    console.log("apna College");
+}, 2000);
+
+clearInterval(id);
